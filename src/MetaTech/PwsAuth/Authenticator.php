eb7ede29deef5e34e0c6a27c5f9503acf902e84d
a-sansara <github@a-sansara.net>
2024-01-05 19:40:15 +0100

    fix phpstan warn for php8

--- src/MetaTech/PwsAuth/Authenticator.php
+++ src/MetaTech/PwsAuth/Authenticator.php
@@ -303,18 +303,16 @@ class Authenticator
         try {
             if (is_null($headers)) {
                 if (!function_exists('apache_request_headers()')) {
-                    function apache_request_headers()
-                    {
-                        foreach($_SERVER as $key=>$value) {
-                            if (substr($key,0,5)=="HTTP_") {
-                                $key=str_replace(" ","-",ucwords(strtolower(str_replace("_"," ",substr($key,5)))));
-                            }
-                            $out[$key]=$value;
+                    $headers = [];
+                    foreach($_SERVER as $key=>$value) {
+                        if (substr($key,0,5)=="HTTP_") {
+                            $key=str_replace(" ","-",ucwords(strtolower(str_replace("_"," ",substr($key,5)))));
                         }
-                        return $out;
+                        $headers[$key]=$value;
                     }
+                } else {
+                    $headers = apache_request_headers();
                 }
-                $headers = apache_request_headers();
             }
             if (isset($headers[$this->config['header']['auth']]) && isset($headers[$this->config['header']['ident']])) {
                 $tokenValue = $headers[$this->config['header']['auth']];
